6a93036080a8ee022025db845d2b4ab8d77a6247
Jeongeum <wjddma1202@naver.com>
2022-11-04 16:48:57 +0900

    rename : 파일 위치 이동

--- js/index.js
+++ js/index.js
@@ -1,41 +1,11 @@
-const drinkItems = document.querySelectorAll(".drink-item"); // 자판기 음료수 아이템
-const returnBtn = document.querySelector(".return-price"); // 반환 버튼
+const drinkItems = document.querySelectorAll(".get-item-btn"); // 자판기 음료수 아이템
+const returnBtn = document.querySelector(".return-btn"); // 반환 버튼
 const insertBtn = document.querySelector(".insert-btn"); // 입금 버튼
 const getBtn = document.querySelector(".get-btn"); // 획득 버튼
 const balanceMoney = document.querySelector(".balance-money"); // 잔액
 const haveMoney = document.querySelector(".have-money"); // 소지금
-const insertMoney = document.querySelector(".insert-price"); // 입금액
-const getCart = document.querySelector(".get-cart"); // 벤딩머신 리스트
-
-// 음료 저장 데이터
-let drinkData = {
-  Origin_Cola: {
-    price: 1000,
-    count: 5,
-  },
-  Violet_Cola: {
-    price: 1000,
-    count: 5,
-  },
-  Yellow_Cola: {
-    price: 1000,
-    count: 5,
-  },
-  Cool_Cola: {
-    price: 1000,
-    count: 5,
-  },
-  Green_Cola: {
-    price: 1000,
-    count: 5,
-  },
-  Orange_Cola: {
-    price: 1000,
-    count: 5,
-  },
-};
-
-// 음료 데이터 localStorage에 저장하기
+const insertMoney = document.querySelector(".insert-inp"); // 입금액
+const getCart = document.querySelector(".get-cart-list"); // 벤딩머신 리스트
 
 // 함수
 function select() {
@@ -75,3 +45,5 @@ drinkItems.forEach((item) => {
 getBtn.addEventListener("click", (e) => {
   e.preventDefault();
 });
+
+console.log(balanceMoney.textContent);
